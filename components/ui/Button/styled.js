f2a49f0e13b0112308c35f556a7359af00938754
Mateo Olarte <mateo.olarte8@gmail.com>
2021-07-02 17:56:09 -0500

    refactor Button component

--- components/ui/Button/styled.js
+++ components/ui/Button/styled.js
@@ -1,59 +1,41 @@
 // vendors
 import styled from "styled-components";
 
+function styles(props) {
+  return `
+    display: inline-block;
+    padding: ${props.theme.sizes.space4} ${props.theme.sizes.space1};
+    border: ${props.white ? `1px solid ${props.theme.colors.secondary}` : 0};
+    border-radius: ${props.theme.sizes.space1};
+    background: ${
+      props.white
+        ? `${props.theme.colors.white}`
+        : `
+          linear-gradient(
+            ${props.theme.colors.primary},
+            ${props.theme.colors.secondary}
+          );
+          `
+    };
+    color: ${
+      props.white
+        ? `${props.theme.colors.secondary}`
+        : `${props.theme.colors.white}`
+    };
+    font-size: 1rem;
+    font-weight: ${props.theme.fonts.medium};
+    line-height: 1.2;
+    text-transform: uppercase;
+    cursor: pointer;
+    outline: none;
+  `;
+}
+
 export const ContainerBtn = styled.button`
-  display: inline-block;
-  padding: ${(props) => props && props.theme.sizes.space4}
-    ${(props) => props && props.theme.sizes.space1};
-  border: ${(props) =>
-    props && props.white ? `1px solid ${props.theme.colors.secondary}` : 0};
-  border-radius: ${(props) => props && props.theme.sizes.space1};
-  background: ${(props) =>
-    props && props.white
-      ? `${props.theme.colors.white}`
-      : `
-        linear-gradient(
-          ${props.theme.colors.primary},
-          ${props.theme.colors.secondary}
-        );
-  `};
-  color: ${(props) =>
-    props && props.white
-      ? `${props.theme.colors.secondary}`
-      : `${props.theme.colors.white}`};
-  font-size: 1rem;
-  font-weight: ${(props) => props && props.theme.fonts.medium};
-  line-height: 1.2;
-  text-transform: uppercase;
-  cursor: pointer;
-  outline: none;
+  ${(props) => styles(props)}
 `;
 
 export const ContainerLink = styled.a`
-  display: inline-block;
-  padding: ${(props) => props && props.theme.sizes.space4}
-    ${(props) => props && props.theme.sizes.space1};
-  border: ${(props) =>
-    props && props.white ? `1px solid ${props.theme.colors.secondary}` : 0};
-  border-radius: ${(props) => props && props.theme.sizes.space1};
-  background: ${(props) =>
-    props && props.white
-      ? `${props.theme.colors.white}`
-      : `
-        linear-gradient(
-          ${props.theme.colors.primary},
-          ${props.theme.colors.secondary}
-        );
-  `};
-  color: ${(props) =>
-    props && props.white
-      ? `${props.theme.colors.secondary}`
-      : `${props.theme.colors.white}`};
-  font-size: 1rem;
-  font-weight: ${(props) => props && props.theme.fonts.medium};
-  line-height: 1.2;
-  text-transform: uppercase;
-  cursor: pointer;
-  outline: none;
+  ${(props) => styles(props)}
   text-decoration: none;
 `;

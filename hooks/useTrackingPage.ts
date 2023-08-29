c0b9e82b38681e956265d0de4a5aaac327a6f1d1
Mateo Olarte <mateo.olarte8@gmail.com>
2023-08-28 23:24:51 -0500

    add new landing page with nextjs 13

--- hooks/useTrackingPage.ts
+++ hooks/useTrackingPage.ts
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import ReactGA from 'react-ga';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import ReactGA from "react-ga";
 
-const gaTrackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
 const ENV = process.env.NODE_ENV;
+const ANALYTICS_KEY = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY;
 
 export function useTrackingPage() {
   const router = useRouter();
@@ -13,10 +13,10 @@ export function useTrackingPage() {
   }
 
   useEffect(() => {
-    if (ENV !== 'development') {
-      ReactGA.initialize(gaTrackingId);
+    if (ENV !== "development" && ANALYTICS_KEY) {
+      ReactGA.initialize(ANALYTICS_KEY);
 
-      router.events.on('routeChangeComplete', handleRouteChangeComplete);
+      router.events.on("routeChangeComplete", handleRouteChangeComplete);
     }
   }, [router]);
 }

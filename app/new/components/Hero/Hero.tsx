6653451ced9243d98f8d23a9027afff4d81223d7
Mateo Olarte <mateo.olarte@dowjones.com>
2023-12-28 14:38:04 -0500

    add validation to missing content from strapi

--- app/new/components/Hero/Hero.tsx
+++ app/new/components/Hero/Hero.tsx
@@ -4,31 +4,35 @@ import Image from "next/image";
 
 export function Hero(props) {
   const { title, description, cta, image } = props;
-  const imageData = image.data.attributes;
+  const imageData = image?.data?.attributes;
   const imageUrl = imageData?.url;
-  const imageWidth = imageData.width;
-  const imageHeight = imageData.height;
-  const imageName = imageData.name;
+  const imageWidth = imageData?.width;
+  const imageHeight = imageData?.height;
+  const imageName = imageData?.name;
 
   return (
     <section className="hero">
       <div className="hero-container">
         <h1 className="hero-title">{title}</h1>
         <p className="hero-description">{description}</p>
+
         {cta && (
           <>
             {cta?.title && <p className="hero-ctaTitle">{cta?.title}</p>}
-            <Button className="hero-cta">{cta.textBtn}</Button>
+            <Button className="hero-cta">{cta?.textBtn}</Button>
           </>
         )}
-        <Image
-          src={imageUrl}
-          width={imageWidth}
-          height={imageHeight}
-          alt={imageName}
-          className="hero-image"
-          priority
-        />
+
+        {imageUrl && (
+          <Image
+            src={imageUrl}
+            width={imageWidth}
+            height={imageHeight}
+            alt={imageName}
+            className="hero-image"
+            priority
+          />
+        )}
       </div>
     </section>
   );

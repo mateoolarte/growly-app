2298dea0a768c16a563c9c9f4eda4a8564f854a1
Mateo Olarte <mateo.olarte8@gmail.com>
2024-03-19 11:09:54 -0500

    feat: finish mercadopago integration for one payment plans

--- app/api/process-payment/route.ts
+++ app/api/process-payment/route.ts
@@ -2,6 +2,9 @@ import { MercadoPagoConfig, Payment } from "mercadopago";
 import { MERCADO_PAGO_ACCESS_KEY } from "@/constants/envs";
 
 export async function POST(request: Request) {
+  if (!MERCADO_PAGO_ACCESS_KEY) {
+    throw Error("Mercado Pago access key not seated");
+  }
 
   const requestData = await request.json();
   const {
@@ -11,12 +14,9 @@ export async function POST(request: Request) {
     payment_method_id,
     payer,
     issuer_id,
-    selected_plan
-  } = requestData
+    description,
+  } = requestData;
 
-  if (!MERCADO_PAGO_ACCESS_KEY) {
-    throw Error('Mercado Pago access key not seated')
-  }
   const client = new MercadoPagoConfig({
     accessToken: MERCADO_PAGO_ACCESS_KEY,
     options: { timeout: 5000 },
@@ -27,11 +27,11 @@ export async function POST(request: Request) {
     const transaction = await payment.create({
       body: {
         transaction_amount,
+        description,
         payment_method_id,
         installments,
         issuer_id,
         token,
-        description: selected_plan,
         payer,
       },
     });
@@ -39,6 +39,6 @@ export async function POST(request: Request) {
     return Response.json({ ok: true, id: transaction.id });
   } catch (error) {
     console.log({ error });
-    throw Error('ERROR Creating payment', { cause: error })
+    throw Error("ERROR Creating payment", { cause: error });
   }
 }
--- app/services/sendPayment.ts
+++ app/services/sendPayment.ts
@@ -1,16 +1,22 @@
 import { v4 as uuid } from "uuid";
 
 export async function sendPayment(data) {
-  const id = uuid();
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Idempotency-Key": id,
-    },
-    body: JSON.stringify(data),
-  };
-  const res = await fetch("/api/process-payment", options);
-  const json = await res.json();
-  return json;
+  try {
+    const id = uuid();
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Idempotency-Key": id,
+      },
+      body: JSON.stringify(data),
+    };
+
+    const res = await fetch("/api/process-payment", options);
+    const json = await res.json();
+
+    return json;
+  } catch (error) {
+    throw Error("ERROR Sending payment", { cause: error });
+  }
 }
